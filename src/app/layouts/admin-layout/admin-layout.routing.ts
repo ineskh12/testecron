8cb38ea7bf4bf4d2aaf1f30c439f0d8f31acca74
ines khelifi <ines.khelifi.1@esprit.tn>
2020-06-12 13:04:43 +0100

    second commit22

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 
-import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+
 import {TestecronComponent} from '../../pages/testecron/testecron.component';
 
 export const AdminLayoutRoutes: Routes = [
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'testecron',      component: TestecronComponent },
+  { path: 'testecron',      component: TestecronComponent },
 
 ];
